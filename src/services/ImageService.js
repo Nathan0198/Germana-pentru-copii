8f3d4c761df6b97fb7a390c673c323414b8b2c60
Nathan0198 <Nathan0198@users.noreply.github.com>
2025-08-17 18:47:41 +0000

    feat: Add image support for lessons 4-9 in ImageService
    - Added all generated story images to imageMap for lessons 4-9
    - Updated getStoryImage() to handle lessons 1-9 correctly
    - Images now mapped for: Familie, Culorile, Animale, Mâncare, Bunicii, Seara
    - Each lesson has 5 story scene images properly integrated
    - Application should now display generated images in story scenes

--- src/services/ImageService.js
+++ src/services/ImageService.js
@@ -29,6 +29,60 @@ class ImageService {
       'bear_family': require('../../assets/images/lessons/lesson_1/story/bjorn_familie.png'), // Pentru familia de urși
 
       // ================================
+      // LESSON 4 - Familie - Story Images
+      // ================================
+      'bjorn_morning_wakeup': require('../../assets/images/lessons/lesson_4/story/bjorn_morning_wakeup.png'),
+      'papa_greeting_bjorn': require('../../assets/images/lessons/lesson_4/story/papa_greeting_bjorn.png'),
+      'mama_breakfast_ready': require('../../assets/images/lessons/lesson_4/story/mama_breakfast_ready.png'),
+      'emma_smelling_food': require('../../assets/images/lessons/lesson_4/story/emma_smelling_food.png'),
+      'bjorn_looking_breakfast': require('../../assets/images/lessons/lesson_4/story/bjorn_looking_breakfast.png'),
+
+      // ================================
+      // LESSON 5 - Culorile - Story Images
+      // ================================
+      'bjorn_asks_anna_play': require('../../assets/images/lessons/lesson_5/story/bjorn_asks_anna_play.png'),
+      'anna_excited_to_play': require('../../assets/images/lessons/lesson_5/story/anna_excited_to_play.png'),
+      'bjorn_suggests_ball_reading': require('../../assets/images/lessons/lesson_5/story/bjorn_suggests_ball_reading.png'),
+      'emma_loves_books': require('../../assets/images/lessons/lesson_5/story/emma_loves_books.png'),
+      'anna_suggests_reading_together': require('../../assets/images/lessons/lesson_5/story/anna_suggests_reading_together.png'),
+
+      // ================================
+      // LESSON 6 - Animale - Story Images
+      // ================================
+      'bjorn_house_colorful': require('../../assets/images/lessons/lesson_6/story/bjorn_house_colorful.png'),
+      'emma_yellow_walls': require('../../assets/images/lessons/lesson_6/story/emma_yellow_walls.png'),
+      'bjorn_brown_doors': require('../../assets/images/lessons/lesson_6/story/bjorn_brown_doors.png'),
+      'emma_garden_flowers': require('../../assets/images/lessons/lesson_6/story/emma_garden_flowers.png'),
+      'bjorn_everything_colorful': require('../../assets/images/lessons/lesson_6/story/bjorn_everything_colorful.png'),
+
+      // ================================
+      // LESSON 7 - Mâncare - Story Images
+      // ================================
+      'bjorn_hungry': require('../../assets/images/lessons/lesson_7/story/bjorn_hungry.png'),
+      'emma_apples_cheese': require('../../assets/images/lessons/lesson_7/story/emma_apples_cheese.png'),
+      'mama_soup_bread': require('../../assets/images/lessons/lesson_7/story/mama_soup_bread.png'),
+      'anna_cake': require('../../assets/images/lessons/lesson_7/story/anna_cake.png'),
+      'bjorn_milk': require('../../assets/images/lessons/lesson_7/story/bjorn_milk.png'),
+
+      // ================================
+      // LESSON 8 - Bunicii Dragi - Story Images
+      // ================================
+      'bjorn_excited_grandparents': require('../../assets/images/lessons/lesson_8/story/bjorn_excited_grandparents.png'),
+      'emma_happy_visit': require('../../assets/images/lessons/lesson_8/story/emma_happy_visit.png'),
+      'opa_greeting_grandchildren': require('../../assets/images/lessons/lesson_8/story/opa_greeting_grandchildren.png'),
+      'oma_gifts': require('../../assets/images/lessons/lesson_8/story/oma_gifts.png'),
+      'bjorn_loving_grandparents': require('../../assets/images/lessons/lesson_8/story/bjorn_loving_grandparents.png'),
+
+      // ================================
+      // LESSON 9 - Seara în Familie - Story Images
+      // ================================
+      'evening_approaching': require('../../assets/images/lessons/lesson_9/story/evening_approaching.png'),
+      'emma_curious_evening': require('../../assets/images/lessons/lesson_9/story/emma_curious_evening.png'),
+      'family_watching_tv': require('../../assets/images/lessons/lesson_9/story/family_watching_tv.png'),
+      'anna_brushing_teeth': require('../../assets/images/lessons/lesson_9/story/anna_brushing_teeth.png'),
+      'bjorn_goodnight': require('../../assets/images/lessons/lesson_9/story/bjorn_goodnight.png'),
+
+      // ================================
       // BACKGROUNDS - Fundaluri (pentru viitor)
       // ================================
       // 'castle_background': require('../../assets/images/backgrounds/castle.png'),
@@ -57,11 +111,12 @@ class ImageService {
 
   // Obține imaginea pentru o scenă din poveste
   getStoryImage(lessonId, imageName) {
-    if (lessonId === 1) {
+    // Pentru lecțiile 1-9, imaginile sunt mapate direct în imageMap
+    if (lessonId >= 1 && lessonId <= 9) {
       return this.getImage(imageName);
     }
     
-    // Pentru alte lecții, construiește numele imaginii
+    // Pentru lecțiile viitoare, construiește numele imaginii
     const fullImageName = `lesson_${lessonId}_${imageName}`;
     return this.getImage(fullImageName);
   }
